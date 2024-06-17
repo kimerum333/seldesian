60c4d40721a628a91f880cfa4c73ff5d546d77da
isile <tkdgus6986@naver.com>
2024-06-18 00:37:18 +0900

    2024-06-18 00:37
    서버용 config 추가

---
+++ src/main/java/tales/akiba/config/RootConfig.java
@@ -0,0 +1,43 @@
+package tales.akiba.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@MapperScan
+@ComponentScan
+@Configuration
+public class RootConfig {
+	
+	protected Class<?>[] getRootConfigClasses() {
+	return new Class[] {RootConfig.class};
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+	SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+	sqlSessionFactory.setDataSource(dataSource());
+	return (SqlSessionFactory) sqlSessionFactory.getObject();
+	}
+
+	private DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mysql://127.0.0.1:3306/toy");
+		hikariConfig.setUsername("toy");
+		hikariConfig.setPassword("toy");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
+
+}
---
+++ src/main/java/tales/akiba/config/ServletConfig.java
@@ -0,0 +1,28 @@
+package tales.akiba.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.toy.controller" })
+public class ServletConfig {
+	
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+}
