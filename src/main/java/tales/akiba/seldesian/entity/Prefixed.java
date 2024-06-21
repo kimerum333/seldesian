ba88cb6cd12dfb9ee6b2242bd405840bb0d0eda5
isile <tkdgus6986@naver.com>
2024-06-21 12:12:32 +0900

    2024-06-21 12:12
    엔티피 작업

---
+++ src/main/java/tales/akiba/seldesian/entity/Prefixed.java
@@ -0,0 +1,31 @@
+package tales.akiba.seldesian.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class Prefixed {
+
+	private String prefixed_tag;
+	private int prefixed_magic_grade;
+	private int tag_no;
+
+	@NonNull
+	private int prefixed_no;
+	private String prefixed_name;
+	private String prefixed_effect;
+	private int prefixed_overlapping;
+	
+}
